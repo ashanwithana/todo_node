050e3fd53c7fab50b62e9fa7d663377e7a1516e4
ashanwithana <ashanwithana4@gmail.com>
2025-05-25 00:29:16 +0530

    Add authentication middleware and update todo routes for user-specific access

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -3,20 +3,26 @@ import db from '../db.js'
 
 const router = express.Router()
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
+    const getTodos = db.prepare(`select * from todos where user_id = ?`)
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
+})
+
+router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare(`insert into todos (user_id, task) values (?, ?)`)
+    const result = insertTodo.run(req.userId, task)
+    res.json({ id: result.lastInsertRowid , task, completed: 0 })
 
 })
 
-router.post('/',(req,res)=>{
-    
+router.put('/:id', (req, res) => {
+
 })
 
-router.put('/:id',(req,res)=>{
-    
-})
+router.delete('/:id', (req, res) => {
 
-router.delete('/:id',(req,res)=>{
-    
 })
 
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import path,{dirname} from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/authRoutes.js'
 import todoRoutes from './routes/todoRoutes.js'
+import authMiddleware from './middleware/authMiddleware.js'
 
 const app = express()
 const PORT = process.env.PORT || 8080
@@ -17,7 +18,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/auth', authRoutes)
 
-app.use('/todos', todoRoutes)
+app.use('/todos', authMiddleware ,todoRoutes)
 
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
