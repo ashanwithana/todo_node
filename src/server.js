c021f1cc25ee85a54aa08dcdcad308bd03730dab
ashanwithana <ashanwithana4@gmail.com>
2025-05-24 13:29:48 +0530

    Implement user authentication and todo management routes with database integration

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import path,{dirname} from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 8080
@@ -9,7 +11,13 @@ const __filename = fileURLToPath(import.meta.url)
 
 const __dirname = dirname(__filename)
 
+app.use(express.json())
+
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.use('/auth', authRoutes)
+
+app.use('/todos', todoRoutes)
 
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
